cb0d427c0621ca5a0f5c300205c69a83f54c8aa9
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-17 20:17:03 -0300

    first structures

---
+++ fiap-fintech/src/main/java/RevenueDAO.java
@@ -0,0 +1,14 @@
+
+
+import java.util.List;
+
+import br.com.fiap.fintech.entity.Revenue;
+
+public interface RevenueDAO {
+	void insert (Revenue revenue);
+	List<Revenue> getAll();
+	void remove (int code);
+	Revenue fetchById (int revenueCode);
+	List<Revenue> fetchAllByUserCode(int userCode);
+	void update (Revenue revenue);
+}
