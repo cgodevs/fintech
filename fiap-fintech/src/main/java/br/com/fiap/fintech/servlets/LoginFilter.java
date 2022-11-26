01bb9188a022d2551c31f60eb459476500c9f269
cgodevs <kharol.go@gmail.com>
2022-11-26 12:13:38 -0300

    fix dependency and Filter

--- fiap-fintech/src/main/java/br/com/fiap/fintech/servlets/LoginFilter.java
+++ fiap-fintech/src/main/java/br/com/fiap/fintech/servlets/LoginFilter.java
@@ -1,9 +1,9 @@
 package br.com.fiap.fintech.servlets;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -14,10 +14,15 @@ import javax.servlet.http.HttpSession;
 
 @WebFilter("/login")
 public class LoginFilter extends HttpFilter implements Filter {
-	private static final long serialVersionUID = 1L;
+       
+    public LoginFilter() {
+        super();
+    }
+
+	public void destroy() {
+	}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-
 		HttpServletRequest request  = (HttpServletRequest) servletRequest;
 		String username = request.getParameter("username");
 
@@ -27,4 +32,8 @@ public class LoginFilter extends HttpFilter implements Filter {
 		chain.doFilter(servletRequest, servletResponse);
 		return;
 	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
 }
